e275a2669f79c598f0f20fedaa69b2fdb9ce869e
EvilLogic <evillogic1@gmail.com>
2020-08-17 10:55:56 -0700

    Added an install script

---
+++ install_configs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Just install some stuff, nothing fancy here
+
+# make sure we have git
+sudo apt install git -y
+
+# install zsh
+sudo apt install zsh -y
+cp .zshrc ~/.zshrc
+sudo chsh -s /bin/zsh
+
+# install tmux
+sudo apt install tmux -y
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+cp .tmux.conf ~/.tmux.conf
