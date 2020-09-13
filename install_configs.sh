a9177d2aad2476bd39d8b2205c52ec4148f9f15e
EvilLogic <evillogic1@gmail.com>
2020-09-13 16:57:53 -0700

    Added tmux resurrect!

--- install_configs.sh
+++ install_configs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Just install some stuff, nothing fancy here
+# Move all the configs to the right places and install tools
 
 # make sure we have git
 sudo apt install git -y
@@ -15,3 +15,8 @@ sudo apt install tmux -y
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp .tmux.conf ~/.tmux.conf
 ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+
+# install vim
+sudo apt install vim -y
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
